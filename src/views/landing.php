33a94b2dada6934f4930d864ae4b3532160e624d
Armando <452@cifpceuta.es>
2025-03-18 23:55:14 +0100

    creando la landing y aplicandoles estilos

--- src/views/landing.php
+++ src/views/landing.php
@@ -5,7 +5,11 @@
         <link rel="stylesheet" href="../../PUBLIC/CSS/landing.css">
     </head>
     <section class="hero-section">
-        
+        <article class="content">
+            <h1>Experimenta la <i>Magia</i> <br> del cine</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente velit nostrum voluptates ipsam aut non officiis quidem dolore saepe suscipit sint a distinctio incidunt quisquam est reprehenderit, nisi beatae iste.</p>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Peliculas</a>
+        </article>
     </section>
 
 <?php 
