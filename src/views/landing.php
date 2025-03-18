dd0db48fce4f56f03f591e38e5f82485e68df3ff
Armando <452@cifpceuta.es>
2025-03-19 00:48:17 +0100

    estilando el hero-section de la landing y probando JS

--- src/views/landing.php
+++ src/views/landing.php
@@ -7,8 +7,10 @@
     <section class="hero-section">
         <article class="content">
             <h1>Experimenta la <i>Magia</i> <br> del cine</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente velit nostrum voluptates ipsam aut non officiis quidem dolore saepe suscipit sint a distinctio incidunt quisquam est reprehenderit, nisi beatae iste.</p>
-            <a href="http://" target="_blank" rel="noopener noreferrer">Peliculas</a>
+            <p>Sumérgete en la magia del cine y vive historias inolvidables en la gran pantalla. Desde clásicos atemporales hasta los últimos estrenos, tu próxima aventura te espera. ¡Reserva tus entradas ahora!</p>
+            <article class="enlace">
+                <a href="">Peliculas</a>
+            </article>
         </article>
     </section>
     <script src="../../PUBLIC/JS/script.js"></script>
