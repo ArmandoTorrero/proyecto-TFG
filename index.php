279454ae3a3a8de238bd622787f5b34a2d66d420
Armando <452@cifpceuta.es>
2025-03-19 17:54:02 +0100

    probando las rutas y creacion del archivo .htaccess

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
     require_once 'core/router.php';
     $router = new Router();
 
+    $router->add('/', 'PeliculasController@index');
+
+    echo __DIR__; 
+
 
     $router->dispatch($_SERVER['REQUEST_URI']);
 ?>
--- src/controllers/PeliculasController.php
+++ src/controllers/PeliculasController.php
@@ -1,3 +1,17 @@
 <?php 
     require_once __DIR__ . '/../models/Peliculas.php';
+
+    class PeliculasController{
+        private $peliculasModel; 
+
+        public function __construct(){
+            $this->peliculasModel = new Peliculas();
+        }
+
+        public function index() {
+            require __DIR__ . '/../views/landing.php';
+        }
+    }
+
+
 ?>
