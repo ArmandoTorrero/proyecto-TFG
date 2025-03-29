d8614ee06ecc9cd079b76227ce10f56927c27561
Armando <452@cifpceuta.es>
2025-03-29 23:01:04 +0100

    agregando estilos CSS y estructura de navegación, incluyendo menú desplegable y enlaces

--- src/views/LAYOUTS/main.php
+++ src/views/LAYOUTS/main.php
@@ -9,16 +9,37 @@
         <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+        
         <link rel="stylesheet" href="public/CSS/main.css">
         <title>SportBooking</title>
     </head>
     <body>
         <nav>
-            
+
+            <h1>SpoortBooking</h1>
+
+            <section class="links">
+                <a href="#">Lorem</a>
+                <a href="#">Lorem</a>
+                <a href="#">Lorem</a>
+            </section>
+
+            <section class="nav-links">
+                <a href="#"><i class="bi bi-person"></i></a>
+                <article class="menu-hamburguesa">
+                    <input type="checkbox" name="menu" id="menu">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </article>
+            </section>
+
+            <section class="desplegable"></section>
+
         </nav>
 
         <!-- Aquí se incluirá el contenido de las vistas -->
