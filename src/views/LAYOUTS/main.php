238c1594b8d0447fb3378e039176386a1b6a84a6
Armando <452@cifpceuta.es>
2025-04-04 17:27:25 +0200

    actualizando el diseño y la estructura de la página de inicio, incluyendo cambios en el encabezado, pie de página y estilos de CSS para reflejar el nuevo nombre "CampoLibre"

--- src/views/LAYOUTS/main.php
+++ src/views/LAYOUTS/main.php
@@ -3,43 +3,37 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.4/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-DQvkBjpPgn7RC31MCQoOeC9TI2kdqa4+BSgNMNj8v77fdC77Kj5zpWFTJaaAoMbC" crossorigin="anonymous">
+        <!-- Fuentes de texto -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
-        
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <!--CSS -->
         <link rel="stylesheet" href="public/CSS/main.css">
-        <title>SportBooking</title>
+        <title>CampoLibre</title>
     </head>
     <body>
         <nav>
 
-            <a href="/TFG/">SpoortBooking</a>
+            <a href="/TFG/" class="logo">CampoLibre</a>
 
-            <section class="links">
-                <a href="#">Lorem</a>
-                <a href="#">Lorem</a>
-                <a href="#">Lorem</a>
+            <article class="menu-hamburguesa">
+                <input type="checkbox">
+                <div></div>
+                <div></div>
+                <div></div>
+            </article>
+
+            <section class="desplegable">
+                <a href="">galletas</a>
             </section>
-
-            <section class="nav-links">
-                <a href="#"><i class="bi bi-person"></i></a>
-                <article class="menu-hamburguesa">
-                    <input type="checkbox" name="menu" id="menu">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </article>
-            </section>
-
-            <section class="desplegable"></section>
                 
         </nav>
 
@@ -48,27 +42,11 @@
 
         <footer>
 
-            <section class="enviar-email">
-
-                <article class="enviar-email-content">
-                    <h1>TE MANTENEMOS INFORMADO</h1>
-                    <p>Introduce tu email para enviarte nuevas novedades</p>
-                </article>
-
-                <article class="input-button">
-                    <input type="email" name="email" id="email" placeholder="Introduce tu email">
-                    <button type="submit">Enviar</button>
-                </article>
-
-            </section>
-
-            <hr>
-
             <section class="footer-links">
 
-                <h1>SpoortBooking</h1>
+                <h2>CampoLibre</h2>
                 <article class="links-1">
-                    <a href="#">Inicio</a>
+                    <a href="/TFG/">Inicio</a>
                     <a href="#">Contactanos</a>
                     <a href="#">Preguntas y respuestas</a>
                 </article>
@@ -78,10 +56,17 @@
                     <a href="#">Politica de protección de datos</a>
                 </article>
                 <article class="RRSS">
-                    <a href="#"><i class="bi bi-instagram"></i></a>
-                    <a href="#"><i class="bi bi-whatsapp"></i></a>
-                    <a href="#"><i class="bi bi-envelope"></i></a>
+                    <a href=""><i class="fa-brands fa-whatsapp"></i></a>
+                    <a href=""><i class="fa-solid fa-envelope"></i></a>
+                    <a href=""><i class="fa-brands fa-instagram"></i></a>
                 </article>
+
+            </section>
+
+            <hr>
+
+            <section class="derechos">
+                <p>&copy 2025 CampoLibre. Todos los derechos reservados.</p>
             </section>
         </footer>
 
--- src/views/landing.php
+++ src/views/landing.php
@@ -6,51 +6,6 @@
     </head>
 
     <!-- Hero section -->
-    <section class="hero">
-        <h1>RESERVAR CAMPOS DEPORTIVOS MAS FACIL QUE NUNCA</h1>
-        <p>Descubre la máxima comodidad al reservar campos deportivos de fútbol, ​​pádel, tenis y baloncesto. Nuestra plataforma intuitiva te permite reservar tu plaza en tan solo unos clics.</p>
-        <button><a href="/TFG/reservarCampo" target="_self">Reservar</a></button>
-    </section>
-
-    <section class="content">
-        <h1>Experimenta la reserva en línea sin complicaciones para tus actividades deportivas favoritas</h1>
-        <section class="content">
-
-            <article class="single-content">
-                <article class="icon"><i class="fa-solid fa-futbol"></i></article>
-                <h1>Elige entre opciones de fútbol, ​​pádel, tenis y baloncesto.</h1>
-                <p>Reserva tu campo sin esfuerzo y disfruta de una gran variedad de deportes.</p>
-                <a href="#" target="_self" >Reservar ></a>
-            </article>
-
-            <article class="single-content">
-                <article class="icon"><i class="fa-solid fa-futbol"></i></article>
-                <h1>Elige entre opciones de fútbol, ​​pádel, tenis y baloncesto.</h1>
-                <p>Reserva tu campo sin esfuerzo y disfruta de una gran variedad de deportes.</p>
-                <a href="#" target="_self" >Reservar ></a>
-            </article>
-
-            <article class="single-content">
-                <article class="icon"><i class="fa-solid fa-futbol"></i></article>
-                <h1>Elige entre opciones de fútbol, ​​pádel, tenis y baloncesto.</h1>
-                <p>Reserva tu campo sin esfuerzo y disfruta de una gran variedad de deportes.</p>
-                <a href="#" target="_self" >Reservar ></a>
-            </article>
-
-        </section>
-    </section>
-
-    <section class="reservar-section">
-        <h1>Reserva tu campo deportivo hoy</h1>
-        <p>¡Únete a nosotros ahora para reservar fácilmente tus campos deportivos favoritos y disfrutar del juego!</p>
-        <button><a href="/TFG/reservarCampo" target="_self">Reservar</a></button>
-    </section>
-
-    <section class="PyR-section">
-        <h1>PREGUNTAS Y RESPUESTAS</h1>
-        <p>Encuentre respuestas a sus preguntas sobre reservas, pagos y cancelaciones aquí.</p>
-        <button>Consultar</button>
-    </section>
     
 
     <script type="module" src="public/JS/script.js"></script>
