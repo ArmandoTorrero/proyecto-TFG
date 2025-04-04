44cdb75d82ea2e205a61fcf6f8d3e6f0bcc02537
Armando <452@cifpceuta.es>
2025-04-04 17:52:14 +0200

    mejorando la sección hero de la página de inicio con nuevos estilos y contenido, incluyendo un encabezado, descripción y botón de reserva

--- src/views/landing.php
+++ src/views/landing.php
@@ -6,7 +6,13 @@
     </head>
 
     <!-- Hero section -->
-    
+    <section class="hero">
+        <h1>Reservar campos deportivos <b>más facil que nunca</b></h1>
+        <p>Descubre la máxima comodidad al reservar campos deportivos de fútbol, pádel, tenis y baloncesto. Nuestra plataforma intuitiva te permite reservar tu plaza en tan solo unos clics.</p>
+        <button>
+            <a href="#" target="_self">Reservar</a>
+        </button>
+    </section>
 
     <script type="module" src="public/JS/script.js"></script>
     
