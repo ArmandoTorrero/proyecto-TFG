4100d189ef6a7202f59d0a16745c688748fdf22a
Armando <452@cifpceuta.es>
2025-04-05 16:22:41 +0200

    agregando estructura y estilos para las secciones de campos deportivos y preguntas frecuentes en la vista de camposDeportivos

--- src/views/camposDeportivos.php
+++ src/views/camposDeportivos.php
@@ -6,7 +6,25 @@
         <link rel="stylesheet" href="public/CSS/camposDeportivos.css">
     </head>
 
-    hola
+    <section class="campos-container">
+
+        <article class="campo">
+            <div class="img"></div>
+            <h2>Grupo Junta del Puerto</h2>
+            <button>
+                <a href="" target="_self">Reservar</a>
+            </button>
+        </article>
+
+    </section>
+
+    <section class="PyR">
+        <h1>Todas tus dudas aclaradas</h1>
+        <p>En este apartado resolvemos las dudas acerca de temas de reenbolso, pagos y reserva de pistads deportivas</p>
+        <button>
+            <a href="#" target="_self">Saber mas</a>
+        </button>
+    </section>
 
 <?php 
     $content = ob_get_clean(); 
